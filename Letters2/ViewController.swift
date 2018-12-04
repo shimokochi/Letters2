8011779b6ad21063e9f8d317084d5596d6c1bc57
shimokochi <shimokouti@gmail.com>
2018-12-04 16:29:08 +0900

    Initial Commit

---
+++ Letters2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Letters2
+//
+//  Created by Shimokochi Katsunori on 2018/12/04.
+//  Copyright © 2018 Shimokochi Shoma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
